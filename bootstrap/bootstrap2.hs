f5a09ca25a0d45ff4f9c43a1dd3503d44d16cd90
Thibault CASTILLON <thibault.castillon@epitech.eu>
2023-01-24 04:08:34 +0100

    [ADD] bootstrap2 -> until parsePair

--- bootstrap/bootstrap2.hs
+++ bootstrap/bootstrap2.hs
@@ -44,3 +44,29 @@ parseSome p s = case p s of
   Nothing -> Nothing
 
 -- STEP 4
+
+parseUInt :: Parser Int
+parseUInt s = case parseSome (parseAnyChar ['0'..'9']) s of
+  Just (cs, s') -> Just (read cs, s')
+  Nothing -> Nothing
+
+
+parseInt :: Parser Int
+parseInt s = case (parseChar '-' s) of
+  Just (c, s') -> case (parseSome (parseAnyChar ['0'..'9']) s') of
+    Just (cs, s'') -> Just (read (c:cs), s'')
+    Nothing -> Nothing
+  Nothing -> (parseUInt s)
+
+parsePair :: Parser a -> Parser (a, a)
+parsePair p (s:s') = case s of
+  '(' -> case (p s') of
+    Just (x, e:s'') -> case e of
+      ' ' -> case p s'' of
+        Just (y, e':s''') -> case e' of
+          ')' -> Just ((x,y), s''')
+          _ -> Nothing
+        Nothing -> Nothing
+      _ -> Nothing
+    Nothing -> Nothing 
+  _ -> Nothing
